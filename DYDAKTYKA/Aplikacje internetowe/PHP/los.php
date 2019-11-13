9182dcaaf972cff98c90f2d3db1ec915ef9e4c3d
ako <andrzej.kotwica@gmail.com>
2019-11-13 23:26:42 +0100

    13.11.2019

--- DYDAKTYKA/Aplikacje internetowe/PHP/los.php
+++ DYDAKTYKA/Aplikacje internetowe/PHP/los.php
@@ -1,14 +1,14 @@
 <?php
-echo "<form action='los.php' method='POST'> 
+echo "<form action='los.php' method='POST'>
  <input type='text' name='szukana' value=''>
- <input type='submit' name='przycisk' value='OK'>
+ <input type='submit' name='przycisk' value='OKKKK'>
 </form>
 ";
 if (isset ($_POST['szukana']))
 {$szuk=$_POST['szukana'];
 //srand (time());
-for ($i=0; $i<20; $i++)  {
-$liczby[$i] = rand(0,20);
+  for ($i=0; $i<20; $i++)  {
+    $liczby[$i] = rand(0,20);
 }
 $ile = 0;
 for ($j=0; $j<20; $j++) {
@@ -18,7 +18,7 @@ $ile++;
 }
 if ($ile == 0)
 print ("<P>Liczby $szuk w tablicy nie odnaleziono.");
-else 
+else
 print ("<P>Liczby $szuk w tablicy odnaleziono."."$ile razy.");
 }
 else
